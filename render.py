8bbe5822d9f19d47252b4f3c5cf210373300cb27
E. Almqvist <elalmqvist@gmail.com>
2022-05-05 09:47:25 +0200

    Stuff

--- render.py
+++ render.py
@@ -3,7 +3,7 @@ import math
 
 class EquationScene(Scene):
     def construct(self):
-        ax = Axes(x_range=[-100, 100, 10], y_range=[-100, 100, 10], axis_config={"include_tip": False})
+        ax = Axes(x_range=[-100, 100, 50], y_range=[-100, 100, 50], x_length=10, y_length=10, axis_config={"include_tip": False})
         self.add(ax)
         self.wait(4)
 
