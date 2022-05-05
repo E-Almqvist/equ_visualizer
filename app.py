af9454027c2670781b5cc4b3166db5461b9357f9
E. Almqvist <elalmqvist@gmail.com>
2022-05-05 11:24:49 +0200

    Visual stuff

--- app.py
+++ app.py
@@ -4,6 +4,8 @@
 import pygame 
 from equation import *
 from render import *
+import math
+import numpy as np
 
 #class FunnelScene(Scene):
 #    def construct(self):
@@ -14,20 +16,22 @@ from render import *
 #        self.play(Transform(sq, cir))
 #        self.play(FadeOut(sq))
 
-equ = Function( lambda x,y: x**2 + y, 2 )
-equ_der = Function( lambda x,y: 2*x + 1, 2 )
+equ = Function( lambda x,y: math.sin(2*x) + y, 2 )
+equ_der = Function( lambda x,y: x+y, 2 )
 
 my_ode = ODE([equ, equ_der])
 
+print("Composing points... ")
 points = []
-for i in range(0, 100):
+num_points = 1600
+for i in np.arange(0, num_points, 0.01):
     points.append(my_ode.get_point(i, i))
 
 space = RenderSpace()
 
 point_objs = []
 for p in points:
-    point_objs.append( Point_2D(space, p[0], p[1]) )
+    point_objs.append( Point_2D(space, p[0], p[1], (255-p[1], p[0], p[1])) )
 
 space.points = point_objs
-space.render()
+space.render(10)
--- render.py
+++ render.py
@@ -14,34 +14,43 @@ class Window:
         self.h = h
         self.run = False
     
-    def open(self):
-        pygame.init()
-        self.window = pygame.display.set_mode( (self.w, self.h) )
-        self.run = True
-        while self.run:
-            for e in pygame.event.get():
-                if e.type == pygame.QUIT:
-                    self.run = False
-            self.window.fill(0)
-
 class Point_2D:
-    def __init__(self, parent_window, x, y, col=(255, 255, 255)):
-        self.parent_window = parent_window
+    def __init__(self, parent, x, y, col=(255, 255, 255)):
+        self.parent = parent
         self.x = x
         self.y = y
         self.col = col
 
-    def render(self):
-        parent_window.window.set_at((self.x, self.y), self.col)
+    def coord(self, scale=1, offset_x=0, offset_y=0):
+        x_coord = int( scale*(self.x + offset_x) )
+        y_coord = int( scale*(self.y + offset_y) )
+        return (x_coord, y_coord)
+
+    def render(self, window, scale=1):
+        x, y = self.coord(scale, 0, 0)
+        x = int(window.w/2 - x/2)
+        y = int(window.h/2 - y/2)
+        window.window.set_at((x,y), self.col)
 
 class RenderSpace(Window):
     def __init__(self, points=[], **kwargs):
         super().__init__(**kwargs)
         self.points = points
 
-    def render(self):
-        super().open()
-        for p in self.points:
-            p.render()
+    def render(self, scale=1):
+        pygame.init()
+        self.window = pygame.display.set_mode( (self.w, self.h) )
+
+        self.run = True
+        while self.run:
+            for e in pygame.event.get():
+                if e.type == pygame.QUIT:
+                    self.run = False
+
+            self.window.fill(0)
+
+            for p in self.points:
+                p.render(self, scale)
+            pygame.display.flip()
 
 
