8fc8b8436309ff9b0134a02f5986bccd441c0d8e
E. Almqvist <elalmqvist@gmail.com>
2022-05-05 10:21:10 +0200

    Pygame stuff

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 
 #from manim import *
+import pygame 
 from equation import *
+from render import *
 
 #class FunnelScene(Scene):
 #    def construct(self):
@@ -12,16 +14,20 @@ from equation import *
 #        self.play(Transform(sq, cir))
 #        self.play(FadeOut(sq))
 
-if __name__ == "__main__":
-    equ = Function( lambda x,y: x**2 + y, 2 )
-    equ_der = Function( lambda x,y: 2*x + 1, 2 )
+equ = Function( lambda x,y: x**2 + y, 2 )
+equ_der = Function( lambda x,y: 2*x + 1, 2 )
 
-    my_ode = ODE([equ, equ_der])
+my_ode = ODE([equ, equ_der])
 
-    points = []
-    for i in range(0, 10):
-        points.append(my_ode.get_point(i, -i))
+points = []
+for i in range(0, 100):
+    points.append(my_ode.get_point(i, i))
 
-    for point in points:
-        print(f"{point}")
+space = RenderSpace()
 
+point_objs = []
+for p in points:
+    point_objs.append( Point_2D(space, p[0], p[1]) )
+
+space.points = point_objs
+space.render()
--- render.py
+++ render.py
@@ -1,9 +1,47 @@
-from manim import *
-import math
+#from manim import *
+#import math
+import pygame
 
-class EquationScene(Scene):
-    def construct(self):
-        ax = Axes(x_range=[-100, 100, 50], y_range=[-100, 100, 50], x_length=10, y_length=10, axis_config={"include_tip": False})
-        self.add(ax)
-        self.wait(4)
+# class EquationScene(Scene):
+#     def construct(self):
+#         ax = Axes(x_range=[-100, 100, 50], y_range=[-100, 100, 50], x_length=10, y_length=10, axis_config={"include_tip": False})
+#         self.add(ax)
+#         self.wait(4)
+
+class Window:
+    def __init__(self, w: int=1600, h: int=900):
+        self.w = w
+        self.h = h
+        self.run = False
+    
+    def open(self):
+        pygame.init()
+        self.window = pygame.display.set_mode( (self.w, self.h) )
+        self.run = True
+        while self.run:
+            for e in pygame.event.get():
+                if e.type == pygame.QUIT:
+                    self.run = False
+            self.window.fill(0)
+
+class Point_2D:
+    def __init__(self, parent_window, x, y, col=(255, 255, 255)):
+        self.parent_window = parent_window
+        self.x = x
+        self.y = y
+        self.col = col
+
+    def render(self):
+        parent_window.window.set_at((self.x, self.y), self.col)
+
+class RenderSpace(Window):
+    def __init__(self, points=[], **kwargs):
+        super().__init__(**kwargs)
+        self.points = points
+
+    def render(self):
+        super().open()
+        for p in self.points:
+            p.render()
+
 
